0277f0e53501134565260ac051cb294b40d009f9
Shiielty <kazutonine@gmail.com>
2022-09-29 19:36:41 +0700

    refactor(*): use functions to create DOMs

--- src/dom.js
+++ src/dom.js
@@ -1,41 +1,56 @@
-const wrapper = document.createElement("div");
+const createHeader = () => {
+    const header = document.createElement("header");
+    const title = document.createElement("h1");
+    const projectHeader = document.createElement("p");
+    const projectWrapper = document.createElement("div");
+    const projectTitle = document.createElement("p");
+    const addProjectBtn = document.createElement("button");
 
-const header = document.createElement("header");
-const main = document.createElement("main");
+    projectHeader.classList.add("project-header");
+    projectWrapper.classList.add("project-wrapper");
+    addProjectBtn.classList.add("add-project-btn");
+    
+    title.textContent = "To-do List";
+    projectHeader.textContent = "Project";
+    projectTitle.textContent = "Default Project";
+    addProjectBtn.textContent = "+ Add Project";
+    
+    projectWrapper.appendChild(projectTitle);
+    header.appendChild(title);
+    header.appendChild(projectHeader);
+    header.appendChild(projectWrapper);
+    header.appendChild(addProjectBtn)
+    
+    return header;
+}
 
-const title = document.createElement("h1");
-const projectHeader = document.createElement("p");
-const projectWrapper = document.createElement("div");
-const projectTitle = document.createElement("p");
-const addProjectBtn = document.createElement("button");
-const taskWrapper = document.createElement("div");
-const taskTitle = document.createElement("p");
-const addTaskBtn = document.createElement("button");
+const createMain = () => {    
+    const main = document.createElement("main");
+    
+    const taskWrapper = document.createElement("div");
+    const taskTitle = document.createElement("p");
+    const addTaskBtn = document.createElement("button");
+    
+    taskWrapper.classList.add("task-wrapper");
+    addTaskBtn.classList.add("add-task-btn");
+    
+    taskTitle.textContent = "Example Task";
+    addTaskBtn.textContent = "+Add Task";
+    
+    taskWrapper.appendChild(taskTitle);
+    main.appendChild(taskWrapper);
+    main.appendChild(addTaskBtn);
 
-projectHeader.classList.add("project-header");
-projectWrapper.classList.add("project-wrapper");
-addProjectBtn.classList.add("add-project-btn");
-taskWrapper.classList.add("task-wrapper");
-addTaskBtn.classList.add("add-task-btn");
+    return main;
+}
 
-title.textContent = "To-do List";
-projectHeader.textContent = "Project";
-projectTitle.textContent = "Default Project";
-addProjectBtn.textContent = "+ Add Project";
-taskTitle.textContent = "Example Task";
-addTaskBtn.textContent = "+Add Task";
+const initializeWebsite = () => {
+    const wrapper = document.createElement("div");
+    wrapper.appendChild(createHeader());
+    wrapper.appendChild(createMain());
+    
+    return wrapper;
+}
 
-taskWrapper.appendChild(taskTitle);
-main.appendChild(taskWrapper);
-main.appendChild(addTaskBtn);
 
-projectWrapper.appendChild(projectTitle);
-header.appendChild(title);
-header.appendChild(projectHeader);
-header.appendChild(projectWrapper);
-header.appendChild(addProjectBtn)
-
-wrapper.appendChild(header);
-wrapper.appendChild(main);
-
-export { wrapper }
+export { initializeWebsite }
