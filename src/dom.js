74236760812dc7650923f0a13e8ebf6fe14c5ea0
Shiielty <kazutonine@gmail.com>
2022-10-26 21:00:56 +0700

    build(build): update the /dist

--- src/dom.js
+++ src/dom.js
@@ -1,53 +1,38 @@
+const createParagraph = (text) => {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+}
+
 const createHeader = () => {
     const header = document.createElement("header");
-    const title = document.createElement("h1");
-    const projectHeader = document.createElement("p");
-    const projectWrapper = document.createElement("div");
-    const projectTitle = document.createElement("p");
-    const addProjectBtn = document.createElement("button");
+    const title = document.createElement('h1');
+    const subtitle1 = createParagraph("Good Morning, Shiielty!")
+    const subtitle2 = createParagraph("You have 3 task left to do,")
+    const subtitle3 = createParagraph("Let's do it!")
+    const project = document.createElement("button");
 
-    projectHeader.classList.add("project-header");
-    projectWrapper.classList.add("project-wrapper");
-    addProjectBtn.classList.add("add-project-btn");
-    
-    title.textContent = "To-do List";
-    projectHeader.textContent = "Project";
-    projectTitle.textContent = "Default Project";
-    addProjectBtn.textContent = "+ Add Project";
-    
-    projectWrapper.appendChild(projectTitle);
+    title.textContent = "Just Do It List";
+    project.textContent = "Projects"
+
     header.appendChild(title);
-    header.appendChild(projectHeader);
-    header.appendChild(projectWrapper);
-    header.appendChild(addProjectBtn)
-    
+    header.appendChild(subtitle1);
+    header.appendChild(subtitle2);
+    header.appendChild(subtitle3);
+    header.appendChild(project);
+
     return header;
 }
 
-const createMain = () => {    
-    const main = document.createElement("main");
-    
-    const taskWrapper = document.createElement("div");
-    const taskTitle = document.createElement("p");
-    const addTaskBtn = document.createElement("button");
-    
-    taskWrapper.classList.add("task-wrapper");
-    addTaskBtn.classList.add("add-task-btn");
-    
-    taskTitle.textContent = "Example Task";
-    addTaskBtn.textContent = "+Add Task";
-    
-    taskWrapper.appendChild(taskTitle);
-    main.appendChild(taskWrapper);
-    main.appendChild(addTaskBtn);
-
-    return main;
-}
+// const createMain = () => {    
+   
+// }
 
 const initializeWebsite = () => {
     const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper")
     wrapper.appendChild(createHeader());
-    wrapper.appendChild(createMain());
+    // wrapper.appendChild(createMain());
     
     return wrapper;
 }
