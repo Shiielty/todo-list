1f85002f6fe91b128691b88c3f2e0dc91bfd84bd
Shiielty <kazutonine@gmail.com>
2022-10-27 19:14:20 +0700

    feat(*): finished the header section

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,5 @@
+import Menu from './public/hamburger.png';
+
 const createParagraph = (text) => {
     const p = document.createElement("p");
     p.textContent = text;
@@ -10,16 +12,26 @@ const createHeader = () => {
     const subtitle1 = createParagraph("Good Morning, Shiielty!")
     const subtitle2 = createParagraph("You have 3 task left to do,")
     const subtitle3 = createParagraph("Let's do it!")
-    const project = document.createElement("button");
+    const projectNav = document.createElement("nav");
+    const projectTitle = document.createElement("button");
+    const projectMenu = document.createElement("button");
+
+    const menuIcon = new Image();
+    menuIcon.src = Menu;
 
     title.textContent = "Just Do It List";
-    project.textContent = "Projects"
+    projectTitle.textContent = "Projects"
+
+    projectMenu.appendChild(menuIcon);
+
+    projectNav.appendChild(projectTitle);
+    projectNav.appendChild(projectMenu);
 
     header.appendChild(title);
     header.appendChild(subtitle1);
     header.appendChild(subtitle2);
     header.appendChild(subtitle3);
-    header.appendChild(project);
+    header.appendChild(projectNav);
 
     return header;
 }
